53a024e196369211414520de47041c0c008e87b1
kkjun302 <gimjun0507@gmail.com>
2024-12-17 18:10:33 +0900

    Update index.js
    환경 변수(process.env.PORT) 를 통해 일반 PORT 설정을 바꿈. 이렇게 해야지 실행이 된다고 함

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const app = require('./app'); // app.js에서 내보낸 Express 앱 가져오기
-const PORT = 8000;
 
+const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
